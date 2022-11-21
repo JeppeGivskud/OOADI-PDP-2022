961cbce1859b8397c097e2c44a79a0632ffd409d
jeppegivskud <Jgivsk20@student.aau.dk>
2022-11-21 12:58:21 +0100

    Orders er tilbage

--- Orders.py
+++ Orders.py
@@ -1,5 +1,10 @@
 class Orders:
-    pass
+    def __init__(self, order_id):
+        self.order_id = order_id
+        self.tickets = []
 
-class Order(Orders):
-    pass
+    def get_order_id(self):
+        return self.order_id
+
+    def collect_tickets(self, sold_ticket):
+        self.tickets.append(sold_ticket)
--- Test.py
+++ Test.py
@@ -8,10 +8,9 @@ if __name__ == "__main__":
     GUI.connect()
     filename = "TwoTestCostumers.txt"
     with open(filename, "rb") as pickle_file:       # Open the file to read from
-        GUI.send(pickle.load(pickle_file))
-        GUI.disconnect()
         User1 = pickle.load(pickle_file)             # Load first object
-        #User2 = pickle.load(pickle_file)  # Load first object
-
-
+        User2 = pickle.load(pickle_file)  # Load first object
     print(User1.get_user_name())
+
+    GUI.send(User1.get_user_name())
+    GUI.disconnect()
