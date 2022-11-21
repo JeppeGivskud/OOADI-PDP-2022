ccfb21e3178cc6b0e9a5bc3757c7121bd6b57ffd
jeppegivskud <Jgivsk20@student.aau.dk>
2022-11-21 14:40:28 +0100

    UserList kan bruges til at teste brugere

---
+++ ClientJeppeTest.py
@@ -0,0 +1,37 @@
+import socket
+
+# No s.close is needed
+class Client():
+    def __init__(self):
+        self.HOST = '127.0.0.1'
+        self.PORT = 40000
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def connect(self):
+        self.socket.connect((self.HOST, self.PORT))
+
+    def send(self, message):
+        self.socket.sendall(message.encode())
+        self.recieve()
+
+    def recieve(self):
+        data = self.socket.recv(1024)
+        print('Ecco', repr(data))
+        if data.decode() == 'Bye':
+            print("Closing connection")
+
+    def disconnect(self):
+        self.send("Bye")
+
+
+
+
+if __name__=="__main__":
+    C = Client()
+    C.connect()
+    C.send("Cheese")
+    C.send("Ball")
+    C.send("Fish")
+    while True:
+        C.send(input())
+   # C.disconnect()
---
+++ ServerTestJeppe.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+class ClientThread(threading.Thread):
+    def __init__(self, client_socket):
+        threading.Thread.__init__(self)
+        self.csocket = client_socket
+    def run(self):
+        with self.csocket:
+            while True:
+                data = self.csocket.recv(1024)          # data received from client
+                self.check_password(data)
+                self.csocket.sendall(data)          # send back echo string to client
+                if (data.decode() == 'Bye'):
+                    print("Closing connection")
+                    break
+    def check_password(self,username):
+        if username == b'yvonne':
+            self.csocket.sendall(b'password')
+
+if __name__=="__main__":
+    host = 'localhost'
+    port = 40000
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        range=0
+        while range<1:
+            client_socket, client_address = s.accept()   # connect to a new client
+            client_thread = ClientThread(client_socket)  # start a new thread
+            client_thread.start()
+            range=range+1
