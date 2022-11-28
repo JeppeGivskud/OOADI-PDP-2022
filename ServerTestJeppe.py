7da4b3848128e1f715f79f235fd6fb9caf27b8c6
jeppegivskud <Jgivsk20@student.aau.dk>
2022-11-28 08:46:23 +0100

    ost

--- ServerTestJeppe.py
+++ ServerTestJeppe.py
@@ -1,34 +1,39 @@
 import socket
 import threading
-from ListOfUsers import*
+from ListOfUsers import *
+
+
 class ClientThread(threading.Thread):
     def __init__(self, client_socket):
         threading.Thread.__init__(self)
         self.csocket = client_socket
-        self.Database=ListOfUsers #initiere en database som kan tilføje brugerobjekter
-        ListOfUsers.Example() #Laver 4 brugere som kan bruges som eksempler
+        self.Database = ListOfUsers  # initiere en database som kan tilføje brugerobjekter
+        ListOfUsers.Example()  # Laver 4 brugere som kan bruges som eksempler
+
     def run(self):
         with self.csocket:
             while True:
-                data = self.csocket.recv(1024)          # data received from client
+                data = self.csocket.recv(1024)  # data received from client
                 self.check_password(data)
-                self.csocket.sendall(data)          # send back echo string to client
+                self.csocket.sendall(data)  # send back echo string to client
                 if (data.decode() == 'Bye'):
                     print("Closing connection")
                     break
-    def check_password(self,username):
+
+    def check_password(self, username):
         if username == b'yvonne':
             self.csocket.sendall(b'password')
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     host = 'localhost'
     port = 40000
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
-        range=0
-        while range<1:
-            client_socket, client_address = s.accept()   # connect to a new client
+        range = 0
+        while range < 1:
+            client_socket, client_address = s.accept()  # connect to a new client
             client_thread = ClientThread(client_socket)  # start a new thread
             client_thread.start()
-            range=range+1
+            range = range + 1
